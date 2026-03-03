c8c6c7694526830c0df6d09714405c18581172be
Dániel Kasza <kaszadani2004@gmail.com>
2026-03-04 00:23:16 +0100

    added a .js file for navbar

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,11 @@
+﻿import "./navbar.css";
+
+export default function Navbar() {
+    return (
+        <header className="navbar">
+            <button className="login-button">
+                Bejelentkezés
+            </button>
+        </header>
+    );
+}
