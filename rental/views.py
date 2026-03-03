3552e0272d08895d510e98dbae107f10f9230130
martonkocse <martonkocse@gmail.com>
2026-03-03 16:38:22 +0100

    fooldal html es admin link

--- rental/views.py
+++ rental/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("BerAuto backend fut. Admin: /admin/")
+    return render(request, "rental/index.html")
