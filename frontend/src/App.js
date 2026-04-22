61571556658d2d922cd1e5f10b6f77fec774a60a
tuhaaaaaa <kaszadani2004@gmail.com>
2026-04-22 20:07:59 +0200

    added Login screen as a route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import Login from "./pages/login";
 
 function App() {
-    return <Home />;
+    return (
+        <Routes>
+            {/* When the path is "/", show the Home page */}
+            <Route path="/" element={<Home />} />
+
+            {/* When the path is "/login", show the Login page */}
+            <Route path="/login" element={<Login />} />
+        </Routes>
+    );
 }
 
 export default App;
