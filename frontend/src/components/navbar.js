363a14934144c5ff5afc320237dc0dbd465e7ea8
tuhaaaaaa <kaszadani2004@gmail.com>
2026-04-22 19:07:21 +0200

    added home button and functionality

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,11 +1,26 @@
 ﻿import "./navbar.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+    const navigate = useNavigate();
+
+    const goHome = () => {
+        navigate("/api/cars");
+    };
+
     return (
         <header className="navbar">
+            <button className="home-button" onClick={ goHome }>
+                Főoldal
+            </button>
+
             <button className="login-button">
                 Bejelentkezés
             </button>
+
+            
         </header>
+
+
     );
 }
